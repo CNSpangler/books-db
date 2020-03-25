56633db7f5cd0a9133febd52ebc3eb20c3616056
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-25 16:24:13 -0700

    test for books post route

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -1,66 +1,66 @@
-// const { getComment, getTweet } = require('../db/data-helpers');
+const { getBook, getBooks, getAuthor } = require('../db/data-helpers');
 
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-// describe('comment routes', () => {
-//   it('creates a comment', async() => {
-//     const tweet = await getTweet();
+describe('book routes', () => {
+  it('creates a book', async() => {
+    const book = await getBook();
 
-//     return request(app)
-//       .post('/api/v1/comments')
-//       .send({
-//         tweetId: tweet._id,
-//         handle: 'test',
-//         text: 'test 1234'
-//       })
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           _id: expect.any(String),
-//           tweetId: tweet._id,
-//           handle: 'test',
-//           text: 'test 1234',
-//           __v: 0
-//         });
-//       });
-//   });
+    return request(app)
+      .post('/api/v1/books')
+      .send({
+        authorId: 1234,
+        title: 'Best Book Ever',
+        genre: 'Non-Fiction'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          authorId: 1234,
+          title: 'Best Book Ever',
+          genre: 'Non-Fiction',
+          __v: 0
+        });
+      });
+  });
 
-//   it('gets a comment by id', async() => {
-//     const tweet = await getTweet();
-//     const comment = await getComment({ tweetId: tweet._id });
+  // it('gets a comment by id', async() => {
+  //   const tweet = await getTweet();
+  //   const comment = await getComment({ tweetId: tweet._id });
 
-//     return request(app)
-//       .get(`/api/v1/comments/${comment._id}`)
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           ...comment,
-//           tweetId: tweet
-//         });
-//       });
-//   });
+  //   return request(app)
+  //     .get(`/api/v1/comments/${comment._id}`)
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         ...comment,
+  //         tweetId: tweet
+  //       });
+  //     });
+  // });
 
-//   it('updates a comment by id', async() => {
-//     const comment = await getComment();
+  // it('updates a comment by id', async() => {
+  //   const comment = await getComment();
 
-//     return request(app)
-//       .patch(`/api/v1/comments/${comment._id}`)
-//       .send({ text: 'bad!' })
-//       .then(res => {
-//         expect(res.body).toEqual({
-//           ...comment,
-//           text: 'bad!'
-//         });
-//       });
-//   });
+  //   return request(app)
+  //     .patch(`/api/v1/comments/${comment._id}`)
+  //     .send({ text: 'bad!' })
+  //     .then(res => {
+  //       expect(res.body).toEqual({
+  //         ...comment,
+  //         text: 'bad!'
+  //       });
+  //     });
+  // });
 
-//   it('deletes a comment by id', async() => {
-//     const comment = await getComment();
+  // it('deletes a comment by id', async() => {
+  //   const comment = await getComment();
 
-//     return request(app)
-//       .delete(`/api/v1/comments/${comment._id}`)
-//       .then(res => {
-//         expect(res.body).toEqual(comment);
-//       });
-//   });
+  //   return request(app)
+  //     .delete(`/api/v1/comments/${comment._id}`)
+  //     .then(res => {
+  //       expect(res.body).toEqual(comment);
+  //     });
+  // });
 
-// });
+});
