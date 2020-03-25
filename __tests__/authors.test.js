413c0d6572c4d30ea295520a86df2fc3aa4e4316
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-25 15:19:33 -0700

    test for get author by id

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -32,39 +32,19 @@ describe('author routes', () => {
       });
   });
 
-  //   it('gets a tweet by id', async() => {
-  //     const tweet = await getTweet();
-  //     // need comments as well since we are populating in route
-  //     const comments = await getComments({ tweetId: tweet._id });
+  it('gets an author by id', async() => {
+    const author = await getAuthor();
+    const books = await getBooks({ authorId: author._id });
 
-  //     return request(app)
-  //       .get(`/api/v1/tweets/${tweet._id}`)
-  //       .then(res => {
-  //         // {
-  //         //   _id: '1111',
-  //         //   handle: 'spot',
-  //         //   text: 'my tweet',
-  //         //   comments: [
-  //         //     { },
-  //         //     { },
-  //         //     {}
-  //         //   ]
-  //         //   __v: 0
-  //         // }
-  //         expect(res.body).toEqual({
-  //           ...tweet,
-  //           comments // because we populate in route
-  //         });
-  //         // expect(res.body).toEqual({
-  //         //   _id: expect.any(String),
-  //         //   handle: 'test',
-  //         //   text: 'test 1234',
-  //         //   comments: expect.any(Array),
-  //         //   __v: 0
-  //         // });
-  //         // expect(res.body.comments).toEqual(JSON.parse(JSON.stringify(comments)));
-  //       });
-  //   });
+    return request(app)
+      .get(`/api/v1/authors/${author._id}`)
+      .then(res => {
+        expect(res.body).toEqual({
+          ...author,
+          books
+        });
+      });
+  });
 
   //   it('updates a tweet by id', async() => {
   //     const tweet = await getTweet();
