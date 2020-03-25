f2627aaf25a0b52c086035d190519adc84797924
Chelsea Spangler <chelseanspangler@gmail.com>
2020-03-25 14:08:22 -0700

    Author and Book models

--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,5 @@
-const Tweet = require('../lib/models/Tweet');
-const Comment = require('../lib/models/Comment');
+const Book = require('../lib/models/Book');
+const Author = require('../lib/models/Author');
 const chance = require('chance').Chance();
 
 // specifying the number of tweets to create with our seed function
@@ -11,12 +11,13 @@ module.exports = async({ authorsToCreate = 10, booksToCreate = 100 } = {}) => {
   // for each tweet in the mapped array we create a tweet in our mongodb
   const genres = ['Science Fiction', 'Fantasy', 'Non-Fiction', 'YA', 'Horror', 'Romance'];
   const books = await Book.create([...Array(booksToCreate)].map(() => ({
-    title: chance.sentence()
+    title: chance.sentence(),
+    author: chance.sentence(),
     genre: chance.pickone(genres),
   })));
 
-  await Author.create([...Array(authorsToCreate)].map(() => ({
-    name: chance.pickone(tweets)._id,
+  const authors = await Author.create([...Array(authorsToCreate)].map(() => ({
+    tweetId: chance.pickone(tweets)._id,
     handle: chance.pickone(handles),
     text: chance.sentence()
   })));
